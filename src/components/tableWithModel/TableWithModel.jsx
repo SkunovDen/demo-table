0c8478fab63cfb84e0995cb624becbfba6e62f53
den <freeforelectron2@gmail.com>
2022-11-24 17:32:11 +0300

    add multiSelection with hold Ctrl down

--- src/components/tableWithModel/TableWithModel.jsx
+++ src/components/tableWithModel/TableWithModel.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useEffect, useState, useRef } from "react";
 import { getModelFromHtml, isMergedSelected } from "./tableModelUtils";
 
 
 const TableWithModel = (props) => {
     const [tableModel, setTableModel] = useState( [] )
+    const [isCtrlDown, setIsCtrlDown] = useState(false)
 
     useEffect( () => {
         const createModel = () => {
@@ -20,65 +20,37 @@ const TableWithModel = (props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
+    const ref = useRef(null);
+
+    useEffect(() => {
+      ref.current.focus();
+    }, []);
 
 
-    const toggleRow = (rowNum) => {
-        setTableModel( (prevModel) => {
-            let updatedModel = Array.from( prevModel )
 
-            updatedModel[rowNum].forEach( (element, index) => {
-                updatedModel[rowNum][index].selected = !prevModel[rowNum][index].selected
-            });
 
-            return updatedModel
+    const clearSelection = () => {
+        setTableModel( (prev) => {
+            const clearedModel = Array.from( prev )
+
+            clearedModel.forEach( (row, rowIndex) => {
+                row.forEach( (cell, cellIndex) => {
+                    clearedModel[rowIndex][cellIndex].selected = false
+                })
+            } )
+
+            return clearedModel
         })
     }
 
-    const toggleRows = (startRow, rowsCount ) => {
-        const endRow = Number(startRow) + Number(rowsCount)
-
-        for (let toglledRowNum = startRow; toglledRowNum < endRow; toglledRowNum ++){
-            toggleRow( toglledRowNum )
-        }   
-    }
-  
-    const cellClickHandler = ({ target }) => {
-        console.log(' CLICK : ', target)
-///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!
-///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!
-///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!
-        const headerRowsCount = 4
-///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!
-///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!        
-///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!        
-        
-        const rowNum = target.dataset.row
-        // const rowSpan = target.rowSpan
-    
-        if (rowNum > headerRowsCount - 1){
-            const rowSpan = target.rowSpan
-            toggleRows(rowNum, target.rowSpan)
-        } else {
-            console.log(' HEADER ')
-            const colNum = Number(target.dataset.col)
-            toggleColumns(colNum, target.colSpan)
-        }
-    
-    }
-
-
-    ///////////////////////
-
-    const toggleColumn = (colNum) => {
+    const selectColumn = (colNum) => {
         setTableModel( (prevModel) => {
             let updatedModel = Array.from( prevModel )
 
-            updatedModel.forEach( (row, rowIndex) => {
-                
+            updatedModel.forEach( (row, rowIndex) => {   
                 row.forEach( (cell, cellIndex) => {
-                    // console.log(cellIndex, ' :: ', rowIndex, ' :: ', cell)
                     if (cellIndex === colNum) {
-                        cell.selected = !cell.selected
+                        cell.selected = true
                     }
                 })
             });
@@ -87,16 +59,143 @@ const TableWithModel = (props) => {
         })
     }
 
-    const toggleColumns = (startColumn, columnsCount ) => {
+    const selectColumns = (startColumn, columnsCount ) => {
         const endColumn = Number(startColumn) + Number(columnsCount)
 
         for (let toglledColNum = startColumn; toglledColNum < endColumn; toglledColNum ++){
-            toggleColumn( toglledColNum )
+            selectColumn( toglledColNum )
         }   
     }
 
+    const selectRow = (rowNum) => {
+        setTableModel( (prevModel) => {
+            let updatedModel = Array.from( prevModel )
 
-    const test = () => {toggleColumn(12)}
+            updatedModel[rowNum].forEach( (element, index) => {
+                updatedModel[rowNum][index].selected = true
+            });
+
+            return updatedModel
+        })
+    }
+
+    const selectRows = (startRow, rowsCount ) => {
+        const endRow = Number(startRow) + Number(rowsCount)
+
+        for (let toglledRowNum = startRow; toglledRowNum < endRow; toglledRowNum ++){
+            selectRow( toglledRowNum )
+        }   
+    }
+
+    const pureSelectionListener = ({ target }) => {
+        if( (target.dataset.col === '0') && (target.dataset.row === '0')){
+            clearSelection()
+            return
+        }
+///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!
+///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!
+///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!
+const headerRowsCount = 4
+///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!
+///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!        
+///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!        
+        
+        if ( !isCtrlDown ){ clearSelection()}
+        const rowNum = target.dataset.row
+        // const rowSpan = target.rowSpan
+    
+        if (rowNum > headerRowsCount - 1){
+            selectRows(rowNum, target.rowSpan)
+        } else {
+            const colNum = Number(target.dataset.col)
+            selectColumns(colNum, target.colSpan)
+        }
+    }
+
+
+
+
+    // const toggleColumn = (colNum) => {
+    //     setTableModel( (prevModel) => {
+    //         let updatedModel = Array.from( prevModel )
+
+    //         updatedModel.forEach( (row, rowIndex) => {
+                
+    //             row.forEach( (cell, cellIndex) => {
+    //                 // console.log(cellIndex, ' :: ', rowIndex, ' :: ', cell)
+    //                 if (cellIndex === colNum) {
+    //                     cell.selected = !cell.selected
+    //                 }
+    //             })
+    //         });
+
+    //         return updatedModel
+    //     })
+    // }
+
+    // const toggleColumns = (startColumn, columnsCount ) => {
+    //     const endColumn = Number(startColumn) + Number(columnsCount)
+
+    //     for (let toglledColNum = startColumn; toglledColNum < endColumn; toglledColNum ++){
+    //         toggleColumn( toglledColNum )
+    //     }   
+    // }
+
+    // const toggleRow = (rowNum) => {
+    //     setTableModel( (prevModel) => {
+    //         let updatedModel = Array.from( prevModel )
+
+    //         updatedModel[rowNum].forEach( (element, index) => {
+    //             updatedModel[rowNum][index].selected = !prevModel[rowNum][index].selected
+    //         });
+
+    //         return updatedModel
+    //     })
+    // }
+
+    // const toggleRows = (startRow, rowsCount ) => {
+    //     const endRow = Number(startRow) + Number(rowsCount)
+
+    //     for (let toglledRowNum = startRow; toglledRowNum < endRow; toglledRowNum ++){
+    //         toggleRow( toglledRowNum )
+    //     }   
+    // }
+  
+//     const cellClickHandler = ({ target }) => {
+//         console.log(' CLICK : ', target)
+// ///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!
+// ///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!
+// ///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!
+//         const headerRowsCount = 4
+// ///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!
+// ///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!        
+// ///REMOVE HARDCODE MAGIC NUMBER !!!!!!!!!!!!!!!!!!        
+        
+//         const rowNum = target.dataset.row
+//         // const rowSpan = target.rowSpan
+    
+//         if (rowNum > headerRowsCount - 1){
+//             toggleRows(rowNum, target.rowSpan)
+//         } else {
+//             console.log(' HEADER ')
+//             const colNum = Number(target.dataset.col)
+//             toggleColumns(colNum, target.colSpan)
+//         }
+    
+//     }
+
+
+
+
+   
+
+
+    const test = () => {
+        clearSelection()
+
+        // selectRow(6)
+
+    }
 
     
     /////////////////////
@@ -126,7 +225,7 @@ const TableWithModel = (props) => {
 
 /// add separated Listeners for header and data areas ???                         
                         // onClick = { (props.row > 3) ? (e) => cellClickHandler(e) : null }
-                        onClick = {(e) => cellClickHandler(e) }
+                        onClick = {(e) => pureSelectionListener(e) }
 /// add separated Listeners for header and data areas ??? 
 
 
@@ -160,14 +259,26 @@ const TableWithModel = (props) => {
         t_container.style.color = t_container.style.color === 'white' ? 'black' : 'white' 
     }
 
-    
+    const keyDownHandler = (e) => {
+        if ( e.key === 'Control'){
+            setIsCtrlDown( (prev) => { return true})
+        }
+    }
+
+
+    const keyReleaseHandler = (e) => {
+        if ( e.key === 'Control'){
+            setIsCtrlDown( (prev) => { return false})
+        }
+    }
+
 
     return(
-        <>                 
+        <div ref={ref} tabIndex={-1}  onKeyDown={(e)=>keyDownHandler(e)} onKeyUp={(e)=>keyReleaseHandler(e)}>                 
             <div id="table-container" className="table-container">
 
                 <table border={1}>
-                    { tableModel.map( (row, rowIndex) => {
+                    { tableModel.map( (_r, rowIndex) => {
                         return (
                             <Row key={rowIndex} row={rowIndex} />
                         )
@@ -177,7 +288,7 @@ const TableWithModel = (props) => {
                 
                 <button onClick={()=>tableContentHide()}>ON | OFF CONTENT</button>
             </div>
-        </>
+        </div>
     )
 
 }
